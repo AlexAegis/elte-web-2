0a8a1a3e4be5ec925aa9cf4de9a3708046fa490f
Győri Sándor <alexaegis@INF.ELTE.HU>
2018-02-27 11:38:47 +0100

    tictac

--- lesson03/script.js
+++ lesson03/script.js
@@ -7,8 +7,10 @@ function $(id) {
 function init() {
 	$('par').addEventListener('click', parClick, false);
 	$('autoComplete').addEventListener('keyup', autoComplete, false);
+	initTicTacToe();
 }
 
+// 2. Feladat
 // A paragrafusra kattintáskor írd ki a konzolra:
 function parClick(e) {
 	// a) az eseményt jelző objektumot;
@@ -31,6 +33,7 @@ function parClick(e) {
 	}
 }
 
+// 3. Feladat
 var movies = [
 	{
 		title: 'Hobbit',
@@ -50,4 +53,55 @@ function autoComplete(e) {
 	$('autoList').innerHTML = movies.filter(movie => $('autoComplete').value != "" && movie.title.includes($('autoComplete').value))
 		.map(movie => "<li>" + movie.title + "</li>")
 		.join("");
+}
+
+// 4. Feladat
+var size = 3
+var currentPlayer = "X"
+var fields = []
+
+function initTicTacToe() {
+	currentPlayer = "X"
+	initDataStructure()
+	renderTicTacTable()
+	for(var fieldRow of fields) {
+		for(var field of fieldRow) {
+			$("field" + field.pos).addEventListener('click', placeTick, false)
+		}
+	}
+}
+
+function initDataStructure() {
+	fields = []
+	for(var i = 0; i < size; i++) {
+		var fieldRow = []
+		for(var j = 0; j < size; j++) {
+			fieldRow.push(
+				{
+					pos: i + "," + j, 
+					val: "a"
+				}
+			)
+		}
+		fields.push(fieldRow);
+	}
+}
+
+function renderTicTacTable() {
+	var board = "<table id=\"tictactoeTable\">"
+	for(var fieldRow of fields) {
+		var row = "<tr>"
+		for(var field of fieldRow) {
+			row = row + "<td id=\"field" + field.pos + "\">" + field.val + "</td>"
+		}
+		board = board + row + "</tr>"
+	}
+	$('tictactoe').innerHTML = board + "</table>"
+}
+
+
+function placeTick(e) {
+	e.target.innerHTML = "X";
+	console.log(e)
+
 }
