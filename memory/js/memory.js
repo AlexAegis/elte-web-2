74a87dad026053dbdadb3758eb256d8ad9bd8008
AlexAegis <alexaegis@gmail.com>
2018-04-11 22:01:51 +0200

    basefield

--- memory/js/memory.js
+++ memory/js/memory.js
@@ -25,8 +25,15 @@ let icons = [
     "umbrella",
     "xbox"
 ]
+// icon for the back of the fields
+let backsideIcon = "expand"
+let iconSize = "5x"
 
 $(document).ready(function () {
+    //let fb = document.getElementById("flipButton")
+
+    //fb.addEventListener("click", () =>  document.getElementById("flipdiv").classList.toggle("flip"))
+
     size = Math.sqrt(icons.length * 2)
     let game = document.getElementById("memoryGame")
     let table = document.createElement("table")
@@ -36,27 +43,50 @@ $(document).ready(function () {
 
     fields = []
     for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            fields.push(new field(new pos(i, j)))
-        }
-    }
-
-    for (let i = 0; i < size; i++) {
         let tableRow = document.createElement("tr")
-        fields.filter(field => field.pos.y === i).forEach(field => {
-            let tableData = document.createElement("td")
-            let icon = document.createElement("i")
-            icon.className += "fas fa-5x fa-camera-retro"
-            field.td = tableData
-            field.td.setAttribute("id", "field" + field.pos)
-            field.td.appendChild(icon)
-            tableRow.appendChild(tableData)
-        })
+        for (let j = 0; j < size; j++) {
+            let field = createFlippableField(new pos(i, j), "camera-retro")
+            fields.push(field)
+            tableRow.appendChild(field.td)
+        }
         tableBody.appendChild(tableRow)
     }
     game.appendChild(table)
 });
 
+function createFlippableField(pos, icon) {
+    let fld = new field(pos);
+
+    let backicon = document.createElement("span")
+    backicon.className += "fas fa-" + iconSize + " fa-" + backsideIcon
+    let backside = document.createElement("div")
+    backside.className += "back"
+    backside.appendChild(backicon)
+
+    let fronticon = document.createElement("span")
+    fronticon.className += "fas fa-" + iconSize + " fa-" + icon
+    let frontside = document.createElement("div")
+    frontside.className += "front"
+    frontside.appendChild(fronticon)
+
+    let flipper = document.createElement("div")
+    flipper.className += "flipper"
+    flipper.appendChild(frontside)
+    flipper.appendChild(backside)
+
+    let flippable = document.createElement("div")
+    flippable.className += "flip-container"
+    flippable.appendChild(flipper)
+
+    fld.td = document.createElement("td")
+    fld.td.setAttribute("id", "field" + fld.pos)
+    fld.td.appendChild(flippable)
+
+    //flippable.addEventListener("click", fld.flip())
+    fld.flp = flippable
+    fld.td.addEventListener("click", fld.flip())
+    return fld;
+}
 
 class field {
     constructor(pos) {
@@ -67,7 +97,18 @@ class field {
         return "pos: " + this.pos.toString() + ", mark: " + this.mark
     }
 
+    flip() {
+        //this.flp.toggleClass('hover')
+        console.log("FLIIII")
+    }
 }
+
+
+$(document).on("click", ".flip-container", function () {
+    $(this).toggleClass('hover')
+    console.log(this.flip())
+});
+
 
 class pos {
     constructor(x, y) {
