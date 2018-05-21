cadd5440c8012e682c80f7c42c5c9697d966d328
AlexAegis <alexaegis@gmail.com>
2018-05-21 18:13:38 +0200

    login done

--- library/js/main.js
+++ library/js/main.js
@@ -1,3 +1,26 @@
+$(document).ready(init());
+
+function init() {
+	$.ajax({
+		type: "POST",
+		url: window.location.pathname + '/class/session.php',
+		data: {
+			action: 'login'
+		},
+		success: function(data) {
+			console.log('session response:' + data);
+			if (data === 'logged') {
+				$('body').load(window.location.pathname + '/content/main.html', data, () => {
+					console.log('MAIN BODY LOADED')
+				});
+			} else {
+				$('body').load(window.location.pathname + '/content/login.html', data, () => {
+					console.log('LOGIN BODY LOADED')
+				});
+			}
+		}
+	});
+}
 
 function logout() {
 	$.ajax({
