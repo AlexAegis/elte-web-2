9b12606d2131af38e94997a5ebbad765f6ca60e7
AlexAegis <alexaegis@gmail.com>
2018-05-23 01:55:51 +0200

    responsive registration screen

--- library/content/registration.php
+++ library/content/registration.php
@@ -1,17 +1,55 @@
-<form id="registrationForm" method="post">
-    <h2>Registration</h2>
-    <input id="registrationMode" hidden>
-    <label id="registrationNameLabel" for="name">Name:</label>
-    <input id="registrationName" name="name" type="text" placeholder="Enter your name" required/>
-    <br/>
-    <label id="registrationEmailLabel" for="email">E-mail:</label>
-    <input id="registrationEmail" name="username" type="email" placeholder="Enter email" required/>
-    <br/>
-    <label id="registrationPasswordLabel" for="password">Password:</label>
-    <input id="registrationPassword" name="password" type="password" placeholder="Enter password" required/>
-    <br/>
-    <button id="register" type="submit" onclick="$('#registrationMode').val('register')">Register</button>
+<form id="registrationForm" method="post" class="form-horizontal my-2 my-lg-0 row" action="../../index.php">
+    <h1 class="display-3 col-12">Registration</h1>
+    <div class="col-sm-8 col-md-8 col-lg-8 col-12  form-group">
+        <label id="registrationNameLabel"
+               for="name"
+               class="control-label mr-1">
+            Name:
+        </label>
+        <input id="registrationName"
+               name="name"
+               type="text"
+               placeholder="Enter your name"
+               class="form-control"
+               required/>
+
+        <label id="registrationEmailLabel"
+               for="email"
+               class="control-label mr-1">
+            E-mail:
+        </label>
+        <input id="registrationEmail"
+               name="username"
+               type="email"
+               placeholder="Enter email"
+               class="form-control"
+               required/>
+
+        <label id="registrationPasswordLabel"
+               for="password"
+               class="control-label mr-1">
+            Password:
+        </label>
+        <input id="registrationPassword"
+               name="password"
+               type="password"
+               placeholder="Enter password"
+               class="form-control"
+               required/>
+    </div>
+    <div class="mt-4 col-sm-4 col-md-4 col-lg-4 col-12">
+
+        <button id="register"
+                class="form-control btn btn-primary mt-2 mr-1"
+                type="submit"
+                onclick="$('#registrationMode').val('register')">
+            Register
+        </button>
+    </div>
+
+
     <h6 id="registrationMessage" class="error"></h6>
+    <input id="registrationMode" hidden>
 </form>
 <script>
 
--- library/index.php
+++ library/index.php
@@ -1,71 +1,50 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <title>Library</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="Library application">
-    <meta name="author" content="AlexAegis">
-    <link rel="icon" href="resources/book.png">
+    <head>
+        <title>Library</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="description" content="Library application">
+        <meta name="author" content="AlexAegis">
+        <link rel="icon" href="resources/book.png">
 
-    <title>Jumbotron Template for Bootstrap</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/library.css">
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <a class="navbar-brand" href="#">Library</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="navbar" class="collapse navbar-collapse">
+                <div id="navigation" class="mr-auto"></div>
+                <div id="user"></div>
+            </div>
+        </nav>
+        <main role="main">
+            <div class="jumbotron">
+                <div id="content" class="container">
 
-    <link rel="stylesheet" href="css/library.css">
-
-
-    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
-            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>');</script>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/plugins.js"></script>
-    <script src="js/utility.js"></script>
-
-</head>
-
-<body>
-
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <a class="navbar-brand" href="#">Library</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div id="navbar" class="collapse navbar-collapse">
-        <div id="navigation" class=" mr-auto"></div>
-        <div id="user"></div>
-    </div>
-
-</nav>
-
-<main role="main">
-    <div class="jumbotron">
-        <div id="content" class="container">
-
-        </div>
-    </div>
-
-        <div id="subContent" class="container">
-            Woohoo
-        </div>
-
-</main>
-
-
-<footer class="container">
-    <p>&copy; AlexAegis 2018</p>
-</footer>
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-
-<script type="text/javascript" src="js/main.js"></script>
-
-</body>
+                </div>
+            </div>
+            <div id="subContent" class="container">
+                Woohoo
+            </div>
+        </main>
+        <footer class="container">
+            <p>&copy; AlexAegis 2018</p>
+        </footer>
+        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+                integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>');</script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/utility.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+    </body>
 </html>
