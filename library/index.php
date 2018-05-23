631e5fc47f6dbb866d590e495efccbeea36ad17d
AlexAegis <alexaegis@gmail.com>
2018-05-23 11:00:31 +0200

    added navbutton css

--- library/index.php
+++ library/index.php
@@ -15,7 +15,10 @@
     </head>
     <body>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <a class="navbar-brand text-white" onclick="init()">Library</a>
+            <div class="navbar-btn" onclick="init()">
+                <a class="navbar-brand text-white text-center" role="link">Library</a>
+            </div>
+
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
