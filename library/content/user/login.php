c5b98a1163c7cdf589867ccd19a40a000b38feca
AlexAegis <alexaegis@gmail.com>
2018-05-27 12:10:38 +0200

    fixed paging return, offset 1

--- library/content/user/login.php
+++ library/content/user/login.php
@@ -1,23 +1,17 @@
 <form id="loginForm" method="post" class="form-inline my-2 my-lg-0">
     <div class="form-group col-12">
-
         <label id="emailLabel" for="email" class="text-white-50 control-label mr-1">E-mail</label>
         <div class="flex-column">
             <input id="email" name="email" placeholder="Enter email" class="form-control "/>
             <label id="emailError" for="email"
                    class="  error labelBelow  position-absolute mdl-color-text--red ml-1 mb-auto"> </label>
         </div>
-
-
-        <br/>
-
         <label id="passwordLabel" for="password" class="text-white-50 control-label mr-1 ml-1">Password</label>
         <div class="flex-column mb-4 mb-sm-0">
             <input id="password" name="password" type="password" placeholder="Enter password" class="form-control"/>
             <label id="passwordError" for="email"
                    class="error labelBelow  position-absolute mdl-color-text--red ml-1 d-lg-block"></label>
         </div>
-
         <button id="login"
                 type="submit"
                 class="form-control btn btn-outline-success my-2 my-sm-0 ml-2 pt-2">
@@ -26,20 +20,7 @@
         <button id="navigateRegistration"
                 type="button"
                 class="form-control btn btn-outline-light my-2 my-sm-0 ml-2"
-                onclick="	$('#content').load(window.location.pathname + '/content/registration.php', () => { // theres a method called navigateRegistration() but somehow I cant call it from here
-        let email = $('#email').val()
-		let regName = $('#registrationName')
-		if (email.indexOf('@') > -1) {
-			$('#registrationEmail').val(email)
-			regName.val(email.split('@')[0])
-		} else {
-			regName.val(email)
-		}
-		regName.focus()
-		let pass = $('#password').val()
-		$('#registrationPassword').val(pass)
-	})
-	$('.navbar-collapse').collapse('hide')">Register
+                onclick="navReg()">Register
         </button>
 
     </div>
@@ -52,4 +33,7 @@
 			navigateListPage(response)
 		})
 	})
+	function navReg() { // idk why i need this "alias"
+		navigateRegistration();
+	}
 </script>
