958cb284587ccddc0ff2873f98517fb2fdf051e5
Győri Sándor <alexaegis@INF.ELTE.HU>
2018-05-28 11:27:12 +0200

    asd

--- wf2zh/class/sessionController.php
+++ wf2zh/class/sessionController.php
@@ -51,8 +51,8 @@ if (isset($_POST['action'])) {
             } else {
                 R::begin();
                     $shape = R::dispense('alakzatok');
-                    //$shape['id'] = $_POST['id'];
-                    $shape['id'] = ((isset($_POST['id']) && $_POST['id'] != "" && ctype_digit($_POST['id'])) ? $_POST['id'] : null);
+                    $shape['id'] = $_POST['id'];
+                    //$shape['id'] = ((isset($_POST['id']) && $_POST['id'] != "" && ctype_digit($_POST['id'])) ? $_POST['id'] : null);
                     $shape['nev'] = $_POST['nev'];
                     $shape['szelesseg'] = $_POST['szelesseg'];
                     $shape['magassag'] = $_POST['magassag'];
