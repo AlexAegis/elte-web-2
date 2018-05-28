ed275a61e09925849eef9cc684e3e5b31bb03f68
Győri Sándor <alexaegis@INF.ELTE.HU>
2018-05-28 10:28:02 +0200

    asd

--- wf2zh/lista.php
+++ wf2zh/lista.php
@@ -13,21 +13,10 @@
     <meta name="author" content="AlexAegis">
     <link rel="icon" href="wf2zh/resources/icon/book.png">
 
-    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
-          integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="./resources/css/datatables.css"/>
-    <link rel="stylesheet" type="text/css" href="./resources/css/material.min.css"/>
-    <link rel="stylesheet" type="text/css" href="./resources/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.min.css">
+
     <link rel="stylesheet" type="text/css" href="./resources/css/_shape.css">
 
-    <script type="text/javascript" src="./resources/js/modernizr-3.5.0.min.js"></script>
     <script type="text/javascript" src="./resources/js/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="./resources/js/plugins.js"></script>
-    <script type="text/javascript" src="./resources/js/material.min.js"></script>
-    <script type="text/javascript" src="./resources/js/datatables.js"></script>
-    <script type="text/javascript" src="./resources/js/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript" src="./resources/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="./resources/js/_utility.js"></script>
     <script type="text/javascript" src="./resources/js/_zh.js"></script>
 </head>
