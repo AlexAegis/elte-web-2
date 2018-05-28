3398f038198c4d8def099631dfcd8f91997beedc
Győri Sándor <alexaegis@INF.ELTE.HU>
2018-05-28 10:00:54 +0200

    asd

--- wf2zh/lista.php
+++ wf2zh/lista.php
@@ -36,8 +36,15 @@
 <?php
  require_once '/home/hallgatok/alexaegis/www/wf2zh/resources/php/rb-mysql.php';
  require_once '/home/hallgatok/alexaegis/www/wf2zh/class/sessionController.php';
-
-$shapes = R::findAll('alakzatok');
+ 
+ 
+ $shapes = array();
+if(isset($_GET['kedvenc'])) {
+    $shapes = R::findAll('alakzatok', ' kedvenc = :fav ', [ 'fav' => $_GET['kedvenc'] == 'true' ? '1' : '0');
+} else {
+    $shapes = R::findAll('alakzatok');
+}
+echo '<a href="./uj.php">Új alakzat<a>';
 echo '<thead>';
 echo '<th>Név</th>';
 echo '<th>Méret</th>';
