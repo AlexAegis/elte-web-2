a2d02b7338770bbbcbe3d383934c1ce090f721c1
Győri Sándor <alexaegis@INF.ELTE.HU>
2018-05-28 11:48:21 +0200

    asd

--- wf2zh/lista.php
+++ wf2zh/lista.php
@@ -46,7 +46,7 @@ foreach ($shapes as &$shape) {
     echo '<td data-id="'.$shape->id.'">'.$shape['nev'].'</td>';
     echo '<td data-id="'.$shape->id.'">'.$shape['szelesseg'].' x '.$shape['magassag'].'</td>';
     echo '<td data-id="'.$shape->id.'">'.($shape['kedvenc']=='1' ? '♥' : '♡').'</td>';
-    echo '<td data-id="'.$shape->id.'">'.'<a href="./megjelenit.php?id='.$shape->id.'">Megjelenít<a></td>';
+    echo '<td data-id="'.$shape->id.'">'.'<a href="megjelenit.php?id='.$shape->id.'">Megjelenít<a></td>';
     echo '</tr>';
 }
 echo '</tbody>';
