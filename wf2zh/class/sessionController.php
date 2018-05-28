6a8f20a44595754cb7b482cba528ecdf1c18fbc5
Győri Sándor <alexaegis@INF.ELTE.HU>
2018-05-28 11:17:30 +0200

    asd

--- wf2zh/class/sessionController.php
+++ wf2zh/class/sessionController.php
@@ -49,14 +49,19 @@ if (isset($_POST['action'])) {
             if (count($errors) > 0) {
                 $result = "error";
             } else {
-                $shape['id'] = $_POST['id'];
-                //$shape['id'] = ((isset($_POST['id']) && $_POST['id'] != "" && ctype_digit($_POST['id'])) ? $_POST['id'] : null);
-                $shape['nev'] = $_POST['nev'];
-                $shape['szelesseg'] = $_POST['szelesseg'];
-                $shape['magassag'] = $_POST['magassag'];
-                $shape['kedvenc'] = isset($_POST['kedvenc']) ? '1' : '0';
-                $shape['alakzat'] = $_POST['alakzat'];
-                R::store($shape);
+    
+                R::transaction(function () {
+                    $shape['id'] = $_POST['id'];
+                    //$shape['id'] = ((isset($_POST['id']) && $_POST['id'] != "" && ctype_digit($_POST['id'])) ? $_POST['id'] : null);
+                    $shape['nev'] = $_POST['nev'];
+                    $shape['szelesseg'] = $_POST['szelesseg'];
+                    $shape['magassag'] = $_POST['magassag'];
+                    $shape['kedvenc'] = isset($_POST['kedvenc']) ? '1' : '0';
+                    $shape['alakzat'] = $_POST['alakzat'];
+                    R::store($shape);
+                });
+
+
                 $other['id'] = $shape->id;
             }
             echo jsonResponse($result, $_POST['action'], $errors, $other);
