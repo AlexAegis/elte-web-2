6a973f268e653faaa4d056427f521e3568daa593
AlexAegis <alexaegis@gmail.com>
2018-06-04 20:51:27 +0200

    asd

---
+++ wf2zh/lista.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>wf2zh</title>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="wf2zh application">
+    <meta name="author" content="AlexAegis">
+    <link rel="icon" href="wf2zh/resources/icon/book.png">
+
+    <link rel="stylesheet" type="text/css" href="./resources/css/_shape.css">
+
+    <script type="text/javascript" src="./resources/js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="./resources/js/_utility.js"></script>
+    <script type="text/javascript" src="./resources/js/_zh.js"></script>
+</head>
+<body>
+<table id="table">
+</table>
+
+<script>
+    $('#table').set('shape', 'list');
+</script>
+</body>
+</html>
---
+++ wf2zh/resources/js/_zh.js
@@ -0,0 +1,91 @@
+$(document).ready(init())
+
+function init(removeParams = false) {
+
+}
+
+jQuery.fn.extend({
+	controller: function (controller, action, onSuccess = null, afterError = null) {
+		let element = this
+		let isForm = element.is('form');
+		let doAjax = function(isForm) {
+			$.ajax({
+				type: 'POST',
+				url: '../php/' + controller + 'Controller.php',
+				data: (isForm ? element.serialize() + '&' : 'value=' + element.val() + '&') + 'action=' + action,
+				success: function (response) {
+					let jsonResponse = JSON.parse(response)
+					switch (jsonResponse.result) {
+						case 'error':
+							if (afterError != null) {
+								afterError(jsonResponse)
+							}
+							break
+						case 'success':
+							if (onSuccess !== null) {
+								onSuccess(jsonResponse)
+							}
+							break
+					}
+				}
+			})
+		}
+		if(isForm) {
+			element.submit(function (e) {
+				e.preventDefault()
+				doAjax(isForm);
+			})
+		} else {
+			doAjax(isForm);
+		}
+	},
+	set: function (controller = 'session', action, parameter = null, modifyJson = null, callback = null, selectCallback = null) {
+		let element = this;
+		return $.ajax({
+			type: 'GET',
+			url: '../../php/' + controller + 'Controller.php',
+			data: {
+				action: action,
+				parameter: parameter
+			},
+			success: function (response) {
+				let jsonResponse = JSON.parse(response)
+				if (modifyJson !== null) {
+					jsonResponse.result = modifyJson(jsonResponse)
+				}
+				if (element.is('form')) {
+					element.find(':input:not(:checkbox):not(:button)').each(function () {
+						let input = $(this)
+						input.val(jsonResponse[input.attr('name')])
+					})
+					element.find('select').each(function () {
+						let input = $(this)
+						input.html('');
+						input.set(input.attr('name'), 'retrieveAll', null, null, function () {
+							input.val(jsonResponse[input.attr('name')])
+							if (selectCallback != null) {
+								selectCallback(input)
+							}
+						})
+					})
+					element.find(':input:checkbox').each(function () {
+						let input = $(this)
+						if (jsonResponse[input.attr('name')] === '1') {
+							input.prop('checked', true)
+						}
+					})
+				} else if (element.is('select')) {
+					element.append('<option value=""></option>') // noselectoption
+					jsonResponse.options.forEach(function (option) {
+						element.append('<option value=' + option.id + '>' + option.name + '</option>')
+					})
+				} else {
+					element.html(jsonResponse.result)
+				}
+				if (callback != null) {
+					callback(jsonResponse)
+				}
+			}
+		})
+	}
+})
